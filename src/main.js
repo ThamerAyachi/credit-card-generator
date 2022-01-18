6806c0365bf70434146171a8a8fec3eb1fea722f
ThamerAyachi <thamerjak3@gmail.com>
2022-01-18 12:47:39 +0100

    add sass

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/scss/main.scss'
 
 createApp(App).mount('#app')
